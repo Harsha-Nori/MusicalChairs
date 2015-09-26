47ccfeaa21a44f9d5efedea347e7c8997f52e364
Harsha Nori <harsha@lawn-143-215-205-104.lawn.gatech.edu>
2015-09-26 11:20:35 -0700

    Initial Commit

---
+++ Musical Chairs/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Musical Chairs
+//
+//  Created by Harsha Nori on 9/26/15.
+//  Copyright © 2015 Harsha Nori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Musical Chairs/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Musical Chairs
+//
+//  Created by Harsha Nori on 9/26/15.
+//  Copyright © 2015 Harsha Nori. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
